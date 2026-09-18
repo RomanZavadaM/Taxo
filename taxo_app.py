3686f920520983f10d86e0ddd9876331e1f8bd7e
RomanZavadaM <zavada.roman.m@gmail.com>
2026-09-18 09:40:23 +0300

    chore: bump Taxo 9.1 entrypoint label to r2

--- taxo_app.py
+++ taxo_app.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Taxo 9.1 candidate r1 application entry point."""
+"""Taxo 9.1 candidate r2 application entry point."""
 import main as core
 from work_analysis_ext import install as install_work_analysis
 from activity_register_60 import install as install_activity_register
