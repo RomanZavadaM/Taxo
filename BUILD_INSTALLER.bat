857932072d13ba269e0d8dc5062f6e821388c2d9
RomanZavadaM <zavada.roman.m@gmail.com>
2026-09-18 23:15:32 +0300

    build: update installer helper to r6

--- BUILD_INSTALLER.bat
+++ BUILD_INSTALLER.bat
@@ -11,4 +11,4 @@ if not exist "%ISCC%" (
 )
 "%ISCC%" installer\Taxo.iss
 if errorlevel 1 exit /b 1
-echo Installer ready: release_out\Taxo_v9_1_candidate_r5_Setup_Windows_x64.exe
+echo Installer ready: release_out\Taxo_v9_1_candidate_r6_Setup_Windows_x64.exe
