822274e30a0ec64a870be50d6659756fe73b8054
RomanZavadaM <zavada.roman.m@gmail.com>
2026-09-19 21:25:59 +0300

    release: mark application entry point as Taxo 10.0

--- taxo_app.py
+++ taxo_app.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Taxo 9.1 candidate r9.8 application entry point."""
+"""Taxo 10.0 stable application entry point."""
 import main as core
 from work_analysis_ext import install as install_work_analysis
 from activity_register_60 import install as install_activity_register
