1169ad9a1b9763eb1ecf1ef4dec32ec07c7f8b97
RomanZavadaM <zavada.roman.m@gmail.com>
2026-09-21 11:16:33 +0300

    chore: make application entry point version-neutral

--- taxo_app.py
+++ taxo_app.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Taxo 10.1 candidate application entry point."""
+"""Taxo application entry point. Current version is owned by main.APP_VERSION."""
 import main as core
 from work_analysis_ext import install as install_work_analysis
 from activity_register_60 import install as install_activity_register
