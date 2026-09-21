046ab022f78f084b5fe7c3f6bf664169de8f9ef3
RomanZavadaM <zavada.roman.m@gmail.com>
2026-09-21 16:29:59 +0300

    chore: bump Taxo candidate to 10.2-r4

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ from vehicle_documents import (
     display_date,
 )
 
-APP_VERSION = "10.2-r3"
+APP_VERSION = "10.2-r4"
 APP_DIR = Path(__file__).resolve().parent
 
 # Постійне робоче сховище не залежить від версії програми. Його адресу можна
