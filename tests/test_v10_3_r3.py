f500a6c5938794a528ec712eb6c9260acd17e239
RomanZavadaM <zavada.roman.m@gmail.com>
2026-09-22 09:58:59 +0300

    test: keep r3 checkpoint compatible with r4

--- tests/test_v10_3_r3.py
+++ tests/test_v10_3_r3.py
@@ -47,9 +47,8 @@ def make_db():
 
 
 class TestTaxo103R3AttestationTailFix(unittest.TestCase):
-    def test_version_is_r3(self):
-        self.assertEqual(main.APP_VERSION,"10.3-r3")
-        self.assertEqual(version_from_file(ROOT/"VERSION.txt"),"10.3-r3")
+    def test_r3_checkpoint_stays_immutable_under_later_10_3_revisions(self):
+        self.assertEqual(main.APP_VERSION,version_from_file(ROOT/"VERSION.txt"))
         self.assertEqual(
             start_archive_stem("10.3-r3"),
             "Taxo_v10_3_candidate_r3_START",
